d8b17bb8190cbaf37fc041e17ccd64779ba869c8
Faja-r <143716722+Faja-r@users.noreply.github.com>
2024-08-04 00:10:52 +0500

    new

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dropdown =document.querySelector(".dropdown");
+const dropdown =document.querySelectorAll(".dropdown");
 
 for (let i=0 ; i<dropdown.clientHeight;i++){
     for (currency_code in country_code){
@@ -13,3 +13,44 @@ for (let i=0 ; i<dropdown.clientHeight;i++){
     let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`
     dropdown[i].insertAdjacentHTML("beforeend",optionTag);
 }
+dropdown[i].addEventListener("change", e={
+    loadFlag(e.target);
+    });
+
+function loadFlag(element){
+    for(code in country_code){
+        if(code==element.value){
+            let imgTag =element.parameter.querySelector("img");
+            imgTag.src=`https://flagsapi.com/${country_code[code]}/flat/64.png`;
+        }
+    }
+}
+window.addEventListener("load",()=>{
+    getExchangeRate();
+});
+getbutton.addEventListener("click," e=>{
+e.preventDefault();
+getExchangeRate();
+});
+ const exchangeIcon = document.querySelector(".drop-down .icon");
+ exchangeIcon.addEventListener("click,"()=>{
+    let tempcode= fromCurrency.value;
+    fromCurrency.value=toCurrency.value;
+    toCurrency.value=tempcode;
+ })
+
+ 
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    getExchangeRate= document.querySelector(".exchange-rate");
+    let amountVal= amount.value;
+
+}
+if(amountVal=="" || amountVal=="0"){
+    amount.value="1";
+    amount.value= 1;
+}
+let url= `https://v6.exchangerate-api.com/${apiKey}/latest/USD`;
+fetch(url).then(Response=> Response.json()).then(result=>{
+    let exchangeRate= result.conversion_rates[toCurrency.value]
+})
